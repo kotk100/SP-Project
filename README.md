da12fafcd5f2c5b37dd4e761b2603dfe258e7d9c
kotk100 <kern.ziga@gmail.com>
2016-11-26 19:41:29 +0100

    Phase 1

--- README.md
+++ README.md
@@ -5,8 +5,7 @@ This web app is primarily for students of FRI (support for other faculties can b
 The main features of the app are <sub><sup>(hopefully will be)</sup></sub>:
 - Adding and removing courses from your timetable
 - Exchanging practical classes with other users
-- Calculating the time you should be leaving for faculty based on bus schedule
-- Creating reminders for when you should wake up or start getting ready to leave
+- Calculating the time you should be waking up and leaving for faculty based on bus schedule
 - Importing your timetable based on your student id number
 - If multiple modes of transportation are available suggesting the best one based on weather
 
@@ -14,3 +13,7 @@ This application will be developed and tested on the following browsers:
 - Microsoft Edge
 - Mozilla Firefox
 - Google Chrome
+
+There were no mayor problems with the development of this webpage, just some usual css hurdles to overcome, mainly to do with the layout. The website displays correctly in all of the listed web browsers, but only for recent versions. 
+The two main elements, that took the longest to make where the timetable and the overlay that is used for adding, changing or exchanging courses.
+Only the frontend is implemented, so some features are not yet working and refreshing the page will reset the state.
--- public/javascripts/validation.js
+++ public/javascripts/validation.js
@@ -1,36 +1,54 @@
-function validateEmail(input) {
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    var element = document.getElementById('Email');
+    if(element) element.addEventListener('keydown', validateEmail);
+
+    element = document.getElementById('Username');
+    if(element) element.addEventListener('keydown', validateUsername);
+
+    element = document.getElementById('password');
+    if(element) element.addEventListener('keydown', validatePassword);
+
+    element = document.getElementById('passwordc');
+    if(element) element.addEventListener('keydown', validatePassword);
+
+    element = document.getElementById('terms');
+    if(element) element.addEventListener('change', termsOfService);
+});
+
+function validateEmail(event) {
     var re = /(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/;
-    if(re.test(input.value))
-        input.className = 'valid-true';
+    if(re.test(event.target.value))
+        event.target.className = 'valid-true';
     else
-        input.className = 'valid-false';
+        event.target.className = 'valid-false';
 }
 
 function termsOfService(el) {
-    document.getElementById('submitDis').disabled = !el.checked;
+    document.getElementById('submitDis').disabled = !el.target.checked;
 }
 
-function validateUsername(input) {
+function validateUsername(event) {
     var re = /(?=.{4,20}$)[a-zA-Z0-9]+([-_\.][a-zA-Z0-9]+)*[a-zA-Z0-9]/;
-    if(re.test(input.value))
-        input.className = 'valid-true';
+    if(re.test(event.target.value))
+        event.target.className = 'valid-true';
     else
-        input.className = 'valid-false';
+        event.target.className = 'valid-false';
 }
 
-function confirmPassword(input) {
-    if (input.value != document.getElementById('password').value) {
-        input.setCustomValidity('The two passwords must match.');
+function confirmPassword(event) {
+    if (event.target.value != document.getElementById('password').value) {
+        event.target.setCustomValidity('The two passwords must match.');
     } else {
-        // input is valid -- reset the error message
-        input.setCustomValidity('');
+        // event.target is valid -- reset the error message
+        event.target.setCustomValidity('');
     }
 }
 
-function validatePassword(input) {
+function validatePassword(event) {
     var re = /(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}/;
-    if(re.test(input.value))
-        input.className = 'valid-true';
+    if(re.test(event.target.value))
+        event.target.className = 'valid-true';
     else
-        input.className = 'valid-false';
+        event.target.className = 'valid-false';
 }
